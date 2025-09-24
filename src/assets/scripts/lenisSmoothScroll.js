dab327a97f438ae7b3d2ba5480e5982410e20b04
Shishira Rao H A <raoshishira@gmail.com>
2025-09-24 15:24:00 +0530

    Fixed build errors in frontmatter

--- src/assets/scripts/lenisSmoothScroll.js
+++ src/assets/scripts/lenisSmoothScroll.js
@@ -1,9 +1,24 @@
-import "@styles/lenis.css";
-
-import Lenis from "lenis";
+import Lenis from 'lenis'
 
 // Script to handle Lenis library settings for smooth scrolling
 // https://github.com/darkroomengineering/lenis
-const lenis = new Lenis({
-    autoRaf: true,
-});
+if (typeof window !== 'undefined') {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+    direction: 'vertical', // vertical, horizontal
+    gestureDirection: 'vertical', // vertical, horizontal, both
+    smooth: true,
+    mouseMultiplier: 1, // 0.5
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+  })
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  requestAnimationFrame(raf)
+}
